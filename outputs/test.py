a10817df5055a3f32e301740b235d9f2328e6ac1
shadowcz007 <chizhiwei007@163.com>
2023-11-24 17:52:02 +0800

    1

--- outputs/test.py
+++ outputs/test.py
@@ -39,7 +39,42 @@ def process_image(file_path, start_offset=32, feathering_weight=0.8):
     return result_image
 
 
-result_image=process_image('1.png',-12,0.1)
+
+def crop_image_remove_transparent(image):
+    # 将PIL的Image类型转换为OpenCV的numpy数组
+    image_np = cv2.cvtColor(np.array(image), cv2.COLOR_RGBA2BGRA)
+
+    # 分离图像的RGBA通道
+    rgba = cv2.split(image_np)
+    alpha = rgba[3]
+
+    # 使用阈值将非透明部分转换为纯白色（255），透明部分转换为纯黑色（0）
+    _, mask = cv2.threshold(alpha, 1, 255, cv2.THRESH_BINARY)
+
+    # 查找轮廓
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # 获取最大轮廓的边界框
+    x, y, w, h = cv2.boundingRect(max(contours, key=cv2.contourArea))
+
+    # 使用边界框裁剪图像
+    cropped_image = image_np[y:y+h, x:x+w]
+
+    # 将裁剪后的图像转换为PIL的Image类型
+    result_pil = Image.fromarray(cv2.cvtColor(cropped_image, cv2.COLOR_BGRA2RGBA))
+
+    return result_pil
+
+
+
+
+
+
+# result_image=process_image('1.png',-12,0.1)
+
+image = Image.open('2.png')
+result_image=crop_image_remove_transparent(image)
+
 result_image.save('test.png')
 
 
